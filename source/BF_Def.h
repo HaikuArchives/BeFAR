b184d6d1920033740f28a65d1544373190b85755
Sergei Reznikov <diver@gelios.net>
2015-02-02 11:34:09 +0300

    Remove version string from window tab title

--- source/BF_Def.h
+++ source/BF_Def.h
@@ -6,7 +6,7 @@
 #define APP_SIGNATURE 	"application/x-vnd.Baza.BeFar"
 #define BF_SETTINGS_VERSION	"1"
 
-#define VERSION_STRING	"(Beta 3.n)"
+#define VERSION_STRING	""
 #define VERSION_DATE	"01 Jun 2009"  
 //#define BF_UNSTABLE	
 
